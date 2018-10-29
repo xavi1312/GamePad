c6417fdb4adab613edf1b18a6a751bfcfa00a303
Estefania <estefi.95.c@gmail.com>
2018-10-29 19:22:29 +0100

    Moure comentari

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// Estat del gamepad al navegador
 window.addEventListener("gamepadconnected", function(e) {
     document.write("Nom: "+ e.gamepad.id +"<br>Connectat: "+ e.gamepad.connected +"<br>Botons: "+  e.gamepad.buttons.length +
     "<br>Numero d'eixos: "+ e.gamepad.axes.length);
   });
-// Estat del gamepad al navegador
+  
 // Personatge que es mourà
