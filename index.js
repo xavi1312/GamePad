d1892a700c391ec2f548de857eb41db2c9369f64
Estefania <estefi.95.c@gmail.com>
2018-10-30 15:52:13 +0100

    Afegir posició absoluta al div, moure element en el js

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ info.appendChild(text);
 var x = 200;
 var y = 200;
 var mou = 5;
+console.log(mando);
+var bola = document.getElementById("bola");
 function gamePad(){
     mando = navigator.getGamepads()[0];
     var botons = mando.buttons;
+    if(botons[13].pressed == true){
+        x +=mou;
+        bola.style.top = x+"px";
+    }
+    if(botons[12].pressed == true){
+        x -= mou;
+        bola.style.top = x+"px";
+    }
+    if(botons[15].pressed == true){
+        y += mou;
+        bola.style.left = y+"px";
+    }
+    if(botons[14].pressed == true){
+        y -= mou;
+        bola.style.left = y+"px";
+    }
 }
-setInterval(gamePad(),50);
+setInterval("gamePad()",50);
