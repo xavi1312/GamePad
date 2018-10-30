485d383bfcb7360aec4cf5024e32350949b95807
Estefania <estefi.95.c@gmail.com>
2018-10-30 17:01:25 +0100

    width i height

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
-
-
 // Estat del gamepad al navegador
 var mando = navigator.getGamepads()[0];
 var info = document.getElementById("info-gamepad");
-var text = document.createTextNode("Nom: "+ mando.id +" Botons: "+mando.buttons.length + " Numero eixos: "
-+ mando.axes.length);
+var text = document.createTextNode(
+  "Nom: " +
+    mando.id +
+    " Botons: " +
+    mando.buttons.length +
+    " Numero eixos: " +
+    mando.axes.length
+);
 info.appendChild(text);
 // Personatge que es mourà
 var x = 200;
 var y = 200;
 var mou = 5;
-console.log(mando);
-var bola = document.getElementById("bola");
-function gamePad(){
-    mando = navigator.getGamepads()[0];
-    var botons = mando.buttons;
-    if(botons[13].pressed == true){
-        x +=mou;
-        bola.style.top = x+"px";
-    }
-    if(botons[12].pressed == true){
-        x -= mou;
-        bola.style.top = x+"px";
-    }
-    if(botons[15].pressed == true){
-        y += mou;
-        bola.style.left = y+"px";
-    }
-    if(botons[14].pressed == true){
-        y -= mou;
-        bola.style.left = y+"px";
-    }
+
+var element = document.getElementById("arale");
+
+var w_original = element.clientWidth;
+var h_original = element.clientHeight;
+
+setInterval("gamePad()", 50);
+
+function gamePad() {
+  mando = navigator.getGamepads()[0];
+  var botons = mando.buttons;
+  //Moure
+  if (botons[13].pressed == true) {
+    x += mou;
+    element.style.top = x + "px";
+  }
+  if (botons[12].pressed == true) {
+    x -= mou;
+    element.style.top = x + "px";
+  }
+  if (botons[15].pressed == true) {
+    y += mou;
+    element.style.left = y + "px";
+  }
+  if (botons[14].pressed == true) {
+    y -= mou;
+    element.style.left = y + "px";
+  }
+  midesImatge(botons);
 }
-setInterval("gamePad()",50);
+
+function midesImatge(botons) {
+  //Fer imatge més gran
+  //Fer imatge més petita
+}
