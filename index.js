420fc30ed22cf607957ea066efdb658453ff9293
Estefania <estefi.95.c@gmail.com>
2018-10-30 15:16:47 +0100

    Canviar index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+
+
 // Estat del gamepad al navegador
-window.addEventListener("gamepadconnected", function(e) {
-    document.write("Nom: "+ e.gamepad.id +"<br>Connectat: "+ e.gamepad.connected +"<br>Botons: "+  e.gamepad.buttons.length +
-    "<br>Numero d'eixos: "+ e.gamepad.axes.length);
-  });
-  
+var mando = navigator.getGamepads()[0];
+var info = document.getElementById("info-gamepad");
+var text = document.createTextNode("Nom: "+ mando.id +" Botons: "+mando.buttons.length + " Numero eixos: "
++ mando.axes.length);
+info.appendChild(text);
 // Personatge que es mourà
+var x = 200;
+var y = 200;
+var mou = 5;
+function gamePad(){
+    mando = navigator.getGamepads()[0];
+    var botons = mando.buttons;
+}
+setInterval(gamePad(),50);
