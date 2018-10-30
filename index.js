1e1d1cd87f85f720aa4e62ae3c6ef707342a0f01
EnyaMol <onaienya@gmail.com>
2018-10-30 15:21:06 +0100

    inici proves Enya

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Estat del gamepad al navegador
 // Personatge que es mourà
+var gamepad = navigator.getGamepads()[0];
+var eixos = document.querySelector("#eixos");
+var posx = 200;
+var posy = 200;
+var mov = 5;
+console.log(gamepad);
+
+function Gamepad(){
+    gamepad = navigator.getGamepads()[0];
+    var botons = gamepad.buttons;
+    if(botons[0].pressed){
+        posx+=mov;
+        
+    }
+}
+setInterval("Gamepad()", 50);
+
